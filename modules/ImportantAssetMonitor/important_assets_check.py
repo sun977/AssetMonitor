edccde51f08ff7ce8c57d658c5f32b9f7d35949a
sun977 <1115476069@qq.com>
2025-01-07 22:09:41 +0800

    add insert_key_asset_ip_detail

--- modules/ImportantAssetMonitor/important_assets_check.py
+++ modules/ImportantAssetMonitor/important_assets_check.py
@@ -20,6 +20,17 @@
 from modules.SecAPI.sec.getSecApiClient import *
 from datetime import time, datetime
 from comm.send_mail import *
+from comm.mysql import *
+from modules.ImportantAssetMonitor.config.logger_config import setup_logger
+
+# 全局变量
+current_abs_path = os.path.abspath(__file__)  # 当前文件位置
+current_abs_path_dir = os.path.dirname(current_abs_path)  # 当前目录
+out_dir_path = os.path.abspath(current_abs_path_dir) + '/../../file/ImportantAssetOut/'  # 从当前目录找到输出文件的位置
+out_print_result = []  # 用于发送邮件的数据列表
+
+# 配置日志记录器
+logger = setup_logger()
 
 
 def read_from(file_path):
@@ -47,12 +58,6 @@ def write_something_to(data, file_path):
     file = file_path
     with open(file, 'w') as f:
         f.write(str(data) + '\n')
-
-
-# 全局变量
-cur_path = os.path.dirname(os.path.realpath(__file__))
-out_file = os.path.join(cur_path, "./file/")  # 文件输出
-out_print_result = []
 
 
 # Manager 类
@@ -134,7 +139,7 @@ class Manager:
 def get_ips_all_status(ip_list):
     '''
     :param: ip list
-    :return: 封装IP所有查询的信息
+    :return: 封装IP所有查询的信息 [{},{}]
     '''
     # 初始化类
     M = Manager()
@@ -177,14 +182,17 @@ def get_ips_all_status(ip_list):
     return result_list
 
 
-# 检测IPs的状态告警
+# 检测IP的状态告警
 def check_ips_alert(ip_list):
+    """"
+    逐步检查
+    """
     result_list = get_ips_all_status(ip_list)  # 获取IPs的所有状态
     # print("result_list:", result_list)
-    ip_unonline_list = []
-    ip_nojowto_list = []
-    ip_hasvuln_list = []
-    ip_unlog_list = []
+    ip_unonline_list = []  # 离线IP资产
+    ip_nojowto_list = []  # 没有jowto的IP资产
+    ip_hasvuln_list = []  # 有漏洞的IP资产
+    ip_unlog_list = []  # 没有日志的
     for data in result_list:  # 遍历数据
         # print("数据：", data)
         if data['online_status'] == False:
@@ -254,10 +262,10 @@ def check_ips_alert(ip_list):
                                                          "msg": '缺失' + key + '日志'})
 
     # # 写入文件
-    # write_to(ip_unonline_list, os.path.join(out_file, "ip_unonline_list.txt"))
-    # write_to(ip_nojowto_list, os.path.join(out_file, "ip_nojowto_list.txt"))
-    # write_to(ip_hasvuln_list, os.path.join(out_file, "ip_hasvuln_list.txt"))
-    # write_to(ip_unlog_list, os.path.join(out_file, "ip_unlog_list.txt"))
+    write_to(ip_unonline_list, os.path.join(out_dir_path, "ip_unonline_list.txt"))
+    write_to(ip_nojowto_list, os.path.join(out_dir_path, "ip_nojowto_list.txt"))
+    write_to(ip_hasvuln_list, os.path.join(out_dir_path, "ip_hasvuln_list.txt"))
+    write_to(ip_unlog_list, os.path.join(out_dir_path, "ip_unlog_list.txt"))
 
     return {
         "ip_unonline_list": ip_unonline_list,
@@ -290,378 +298,104 @@ def send_mail(data):
     mail.send_mail(subject, userlist, content)
 
 
-if __name__ == "__main__":
-    test_list = [
-        '10.1.11.11',
-        '10.44.96.180',
-        '10.44.96.183',
-        '10.95.58.94'
-    ]
+# 从数据库中获取IP列表
+def get_ip_from_db():
+    """
+    从数据库中获取IP列表
+    :return:[{},{}]
+    """
+    ip_list_db = []
+    # 获取IP资产监控表 key_asset_ip
+    sql = "SELECT ip, project, owner FROM key_asset_ip where status = '1'"  # 获取监控状态为1的IP
+    res_sql = MySQL(sql=sql).exec()
+    if res_sql.get('state') == 1:
+        logger.info(f"modules.ImportantAssetMonitor.important_asset_check.get_ip_from_db() SQL:{sql} exec Success")
+        ip_list_db = res_sql.get('data')
+    else:
+        logger.warning(
+            f"modules.ImportantAssetMonitor.important_asset_check.get_ip_from_db() SQL:{sql} exec Failed: {res_sql.get('msg')}")
+        ip_list_db = res_sql.get('data')
+    return ip_list_db
 
-    # 定义待检测目标
-    ## crm 系统
-    crm_list = [
-        '10.44.112.7',
-        '10.44.112.17',
-        '10.44.112.18',
-        '10.57.82.42',
-        '10.57.82.43',
-        '10.57.82.44',
-        '10.44.112.20',
-        '10.44.112.21',
-        '10.57.82.45',
-        '10.57.82.46'
-    ]
+# 所有IP的所有状态信息入库
+def insert_key_asset_ip_detail(data):
+    """
+    封装成字典然后插入
+    :param ip_list_db:
+    :return:
+    """
+    # data_dcit = {
+    #     "ip": ip,
+    #     "project": project,
+    #     "owner": owner,
+    #     "online_status": online_status,
+    #     "jowto_status": jowto_status,
+    #     "vuln_status": vuln_status,
+    #     "log_status": log_status,
+    #     "note":note
+    # }
+    data_dit = data
+    try:
+        sql_insert = (
+            "INSERT INTO key_asset_ip_detail (ip, project, owner, online_status, jowto_status, vuln_status, log_status, note) "
+            "VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s') "
+            "ON DUPLICATE KEY UPDATE "
+            "ip='%s',project='%s',owner='%s',online_status='%s',jowto_status='%s',vuln_status='%s',log_status='%s', note='%s', updateTime=CURRENT_TIMESTAMP(6); ;" % (
+                data_dit.get('ip'), data_dit.get('project'), data_dit.get('owner'), data_dit.get('online_status'), data_dit.get('jowto_status'), data_dit.get('vuln_status'), data_dit.get('log_status'), data_dit.get('note'), data_dit.get('ip'), data_dit.get('project'), data_dit.get('owner'), data_dit.get('online_status'), data_dit.get('jowto_status'), data_dit.get('vuln_status'), data_dit.get('log_status'), data_dit.get('note')
+            )
+        )
+        res_sql = MySQL(sql=sql_insert).exec()
+        if res_sql.get('state') == 1:
+            logger.info(f"modules.ImportantAssetMonitor.important_asset_check.insert_key_asset_ip_detail() SQL:{sql_insert} exec Success")
+        else:
+            logger.warning(f"modules.ImportantAssetMonitor.important_asset_check.insert_key_asset_ip_detail() SQL:{sql_insert} exec Failed: {res_sql.get('msg')}")
+        return sql_insert
+    except Exception as e:
+        logger.error(f"modules.ImportantAssetMonitor.important_asset_check.insert_key_asset_ip_detail() Failed to insert data into db: {e}")
+        return sql_insert
 
-    ## 奇迹系统
-    qiji_list = [
-        '10.249.108.9',
-        '10.49.176.24',
-        '10.249.108.10',
-        '10.49.176.25',
-        '10.249.108.11',
-        '10.49.176.26',
-        '10.49.165.64',
-        '10.249.97.165',
-        '10.41.200.165',
-        '10.57.96.77',
-        '10.41.200.166',
-        '10.57.96.78',
-        '10.41.200.167',
-        '10.57.96.79'
-    ]
 
-    ## ERP
-    erp_list = [
-        '10.44.33.18',
-        '10.57.148.40',
-        '10.44.33.14',
-        '10.57.148.36',
-        '10.44.33.20',
-        '10.57.148.42',
-        '10.44.33.12',
-        '10.44.33.15',
-        '10.57.148.34',
-        '10.57.148.37',
-        '10.44.33.17',
-        '10.57.148.39',
-        '10.44.33.13',
-        '10.44.33.19',
-        '10.57.148.35',
-        '10.57.148.41',
-        '10.44.33.16',
-        '10.57.148.38',
-        '10.44.113.20',
-        '10.44.113.18',
-        '10.57.82.39',
-        '10.57.82.40'
-    ]
 
-    ## 江河平台
-    jianghe_list = [
-        '10.49.44.82',
-        '10.49.204.35',
-        '10.49.44.80',
-        '10.49.204.36',
-        '10.49.44.84',
-        '10.49.204.37'
-    ]
 
-    ## MES 系统
-    mes_list = [
-        '10.73.66.7',
-    ]
 
-    ## PDM 系统
-    pdm_list = [
-        '10.57.94.246',
-        '10.57.94.247',
-        '10.57.94.248',
-        '10.57.94.249'
-    ]
-
-    ## PMIS 系统
-    pmis_list = [
-        '10.41.200.12',
-        '10.41.200.14',
-        '10.41.200.17',
-        '10.41.200.40',
-        '10.41.200.42',
-        '10.57.96.43',
-        '10.57.96.44',
-        '10.57.96.45',
-        '10.57.96.46'
-    ]
-
-    ## 许可证 系统
-    license_list = [
-        '10.249.108.20',
-        '10.249.108.19',
-        '10.249.97.169',
-        '10.249.97.170',
-        '10.249.105.10',
-        '10.249.105.9',
-        '10.49.204.29',
-        '10.49.194.58',
-        '10.249.105.246',
-        '10.249.105.89',
-        '10.49.200.141'
-    ]
-
-    ## 倚天 系统
-    yitian_list = [
-        '10.249.43.200',
-        '10.249.106.197',
-        '10.249.106.198',
-        '10.49.40.248',
-        '10.49.173.227',
-        '10.49.32.105',
-        '10.49.165.30',
-        '10.49.173.94',
-        '10.49.172.34'
-    ]
-
-    ## OA 系统
-    oa_list = [
-        '10.249.34.82',
-        '10.249.80.27',
-        '10.249.80.43',
-        '10.249.80.45',
-        '10.249.80.32',
-        '10.249.80.42',
-        '10.249.80.37',
-        '10.249.34.41',
-        '10.249.80.49',
-        '10.249.80.106',
-        '10.249.80.89',
-        '10.249.80.105',
-        '10.249.80.107',
-        '10.249.34.73',
-        '10.249.46.137',
-        '10.249.46.140',
-        '10.249.64.15',
-        '10.249.64.14',
-        '10.249.162.219',
-        '10.49.204.27',
-        '10.249.162.220',
-        '10.49.204.28',
-        '10.249.162.218',
-        '10.249.162.221',
-        '10.49.176.23',
-        '10.249.22.178',
-        '10.46.40.23',
-        '10.46.40.20',
-        '10.46.40.17',
-        '10.46.40.26',
-        '10.46.40.29',
-        '10.46.40.24',
-        '10.41.200.111',
-        '10.46.36.27'
-    ]
-
-    ## 报账 系统
-    baozhang_list = [
-        '10.249.108.6',
-        '10.49.176.17',
-        '10.249.108.7',
-        '10.249.108.8',
-        '10.49.176.12',
-        '10.49.176.13',
-        '10.249.105.29',
-        '10.49.176.8',
-        '10.44.121.70',
-        '10.249.122.10'
-    ]
-
-    ## 财务运营 系统
-    caiyun_list = [
-        '10.249.108.48',
-        '10.249.108.49',
-        '10.49.176.5',
-        '10.249.108.52',
-        '10.49.176.7',
-        '10.249.108.38',
-        '10.49.176.6',
-        '10.249.108.40',
-        '10.49.176.11',
-        '10.249.108.42',
-        '10.49.176.20',
-        '10.249.108.44',
-        '10.249.108.28',
-        '10.49.176.19',
-        '10.249.108.46',
-        '10.49.176.18',
-        '10.249.108.30',
-        '10.49.176.10',
-        '10.249.108.36',
-        '10.249.108.37',
-        '10.49.176.16',
-        '10.249.108.34',
-        '10.49.176.9',
-        '10.249.108.33',
-        '10.49.176.15',
-        '10.249.108.24',
-        '10.49.176.21',
-        '10.249.108.15',
-        '10.49.176.14',
-        '10.49.44.63',
-        '10.49.176.22'
-    ]
-
-    ## 百望 系统
-    baiwang_list = [
-        '10.41.200.127',
-        '10.41.200.129'
-    ]
-
-    ## U8 系统
-    u8_list = [
-        '10.41.200.107',
-        '10.96.20.132',
-        '10.41.200.108',
-        '10.96.21.3',
-        '10.57.96.54',
-        '10.57.96.55'
-    ]
-
-    ## CBS 系统
-    cbs_list = [
-        '10.44.121.90',
-        '10.44.121.154',
-        '10.41.200.150',
-        '10.41.200.149',
-        '10.44.121.156',
-        '10.44.121.155'
-    ]
-
-    ## BI 系统
-    bi_list = [
-        '10.41.200.142',
-        '10.41.76.13',
-        '10.57.96.51'
-    ]
-
-    ## 数据服务 系统
-    data_service_list = [
-        '10.41.76.17',
-        '10.41.76.19',
-        '10.57.96.47'
-    ]
-
-    ## 售后 系统
-    after_sale_list = [
-        '10.48.48.73',
-        '10.48.48.68',
-        '10.48.60.87',
-        '10.48.60.88',
-        '10.48.60.90',
-        '10.48.60.91',
-        '10.48.60.89',
-        '10.48.60.92',
-        '10.48.48.74',
-        '10.48.60.93',
-        '10.44.66.49',
-        '10.41.200.148',
-        '10.41.200.130',
-        '10.41.200.143',
-        '10.41.200.131',
-        '10.41.200.145',
-        '10.41.200.144',
-        '10.41.200.147'
-    ]
-
-    ## SAP 系统
-    sap_list = [
-        '10.44.120.206',
-        '10.44.120.204',
-        '10.44.120.205',
-        '10.44.120.196',
-        '10.44.120.194',
-        '10.44.120.195'
-    ]
-
-    ## 基础建设 系统
-    jijian_list = [
-        '10.249.105.108',
-        '10.249.105.35',
-        '10.249.105.69',
-        '10.49.174.208',
-        '10.249.106.119',
-        '10.249.106.120',
-        '10.249.106.119',
-        '10.249.106.120',
-        '10.249.106.189',
-        '10.249.106.190',
-        '10.49.174.204',
-        '10.249.106.192',
-        '10.249.106.191',
-        '10.49.174.207',
-        '10.249.106.194',
-        '10.249.106.193',
-        '10.49.174.206',
-        '10.249.106.192',
-        '10.249.106.191',
-        '10.49.174.207',
-        '10.49.40.217',
-        '10.49.174.205',
-        '10.49.40.111',
-        '10.49.40.110',
-        '10.49.174.210',
-        '10.49.32.83',
-        '10.49.32.84',
-        '10.49.40.135',
-        '10.49.40.134',
-        '10.49.40.133',
-        '10.49.174.209',
-        '10.49.41.198'
-    ]
-
-    ## 蓝信 系统
-    lx_list = []
-
-    ## 列表装填所有列表
-    all_list = [
-        erp_list,
-        jianghe_list,
-        mes_list,
-        pdm_list,
-        pmis_list,
-        license_list,
-        yitian_list,
-        oa_list,
-        baozhang_list,
-        caiyun_list,
-        baiwang_list,
-        u8_list,
-        cbs_list,
-        bi_list,
-        data_service_list,
-        after_sale_list,
-        sap_list,
-        jijian_list,
-        lx_list
-    ]
-
-    # get_ips_all_status(test_list)
-    # res = check_ips_alert(test_list)
-    # print(res)
-
-    # # # 遍历列表
-    # for list_name in all_list:
-    #     print("LIST_NAME:" ,list_name)
-    #     # 遍历每个列表里面的元素
-    #     for item in list_name:
-    #         print(item)
-    #         res = check_ips_alert(item)
-
+# 运行函数
+def run_important_asset_check():
+    """
+    串联运行函数
+    :return:
+    """
+    # 为了发邮件
     print("------------------------------------------------------------------------")
     # 输出一下时间，便于查询run.log日志
     print("DATE:", datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+    # 改为从数据库中获取IP列表
+    ip_list_db = get_ip_from_db()  # [{},{}]  {'ip': '10.44.112.17', 'project': '奇安信集团- CRM系统采购及规划建设项目', 'owner': '娄卫赢'}
+
+    # 遍历情况发邮件
     # 列表套列表可以直接遍历
-    for item in all_list:
+    for item in ip_list_db:
         # print("item:", item)
-        res = check_ips_alert(item)
+        res = check_ips_alert(item.get('ip'))  # 每个列表元素是字典，取字典的ip值
         # print("列表结果：", res)
     print("out_print_result:", out_print_result)
     send_mail(out_print_result)
+
+
+    # 获取的IP的所有状态同步资产监控表 key_asset_ip_detail
+    # res = get_ips_all_status(ip_list_db)
+    ip_list = []  # 纯IP列表['','']
+    for ite in ip_list_db:
+        ip_list.append(ite.get('ip'))
+
+    res_get_ips_all_status = get_ips_all_status(ip_list)   # [{},{}]  {'ip':'','ipassets_project_name','online_status':'','jowto_status':'','vuln_status':'','log_status':'','log_status_info':''}
+    for x in res_get_ips_all_status:
+        # 需要封装一下 x 数据
+
+    pass
+
+
     print("------------------------------------------------------------------------")
+
+
+if __name__ == "__main__":
+  run_important_asset_check()
--- sync_im_asset.py
+++ sync_im_asset.py
@@ -7,9 +7,9 @@
     date: 2025.01.07
     note: 调用脚本（临时）
 """
-
+from modules.ImportantAssetMonitor.important_assets_check import get_ip_from_db
 from modules.ImportantAssetMonitor.sync.sync_sec_data2db_from_txt import run_sync_sec_data2db_from_txt
-
 
 if __name__ == '__main__':
     run_sync_sec_data2db_from_txt()
+    # print(get_ip_from_db())
