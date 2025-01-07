4caf50fc30546fde94f56a16b9a060acda377c37
sun977 <1115476069@qq.com>
2025-01-07 17:19:58 +0800

    add sync_im_asset.py

---
+++ sync_im_asset.py
@@ -0,0 +1,15 @@
+# !/usr/bin/env python3
+# _*_ coding: utf-8 _*_
+"""
+    auth: sunhaobo
+    version: v1.0
+    function: sync im_asset_ip to key_asset_ip table
+    date: 2025.01.07
+    note: 调用脚本（临时）
+"""
+
+from modules.ImportantAssetMonitor.sync.sync_sec_data2db_from_txt import run_sync_sec_data2db_from_txt
+
+
+if __name__ == '__main__':
+    run_sync_sec_data2db_from_txt()
